2db379e6d51a1a694fba8524f8ba003a52096a49
Tyler <tylfin@gmail.com>
2020-06-29 08:56:28 -0400

    Need to fix compilation error

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,99 @@
 import * as vscode from 'vscode';
+import { GoRunTestCodeLensProvider } from './run';
+import { GO_MODE } from 'Go/src/goMode';
+import { getGoConfig } from 'Go/src/util';
+import { testAtCursor, testCurrentFile, testCurrentPackage, testPrevious, testWorkspace } from 'Go/src/goTest';
+import { cancelRunningTests, showTestOutput } from 'Go/src/testUtils';
+import { toggleCoverageCurrentPackage } from 'Go/src/goCover';
 
 export function activate(context: vscode.ExtensionContext) {
+	const testCodeLensProvider = new GoRunTestCodeLensProvider();
+	context.subscriptions.push(vscode.languages.registerCodeLensProvider(GO_MODE, testCodeLensProvider));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.cursor', (args) => {
+			const goConfig = getGoConfig();
+			testAtCursor(goConfig, 'test', args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.debug.cursor', (args) => {
+			const goConfig = getGoConfig();
+			testAtCursor(goConfig, 'debug', args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.benchmark.cursor', (args) => {
+			const goConfig = getGoConfig();
+			testAtCursor(goConfig, 'benchmark', args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.package', (args) => {
+			const goConfig = getGoConfig();
+			const isBenchmark = false;
+			testCurrentPackage(goConfig, isBenchmark, args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.benchmark.package', (args) => {
+			const goConfig = getGoConfig();
+			const isBenchmark = true;
+			testCurrentPackage(goConfig, isBenchmark, args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.file', (args) => {
+			const goConfig = getGoConfig();
+			const isBenchmark = false;
+			testCurrentFile(goConfig, isBenchmark, args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.benchmark.file', (args) => {
+			const goConfig = getGoConfig();
+			const isBenchmark = true;
+			testCurrentFile(goConfig, isBenchmark, args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.workspace', (args) => {
+			const goConfig = getGoConfig();
+			testWorkspace(goConfig, args);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.previous', () => {
+			testPrevious();
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.coverage', () => {
+			toggleCoverageCurrentPackage();
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.showOutput', () => {
+			showTestOutput();
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('go.test.cancel', () => {
+			cancelRunningTests();
+		})
+	);
+
 	console.log('Activated Go Check for Visual Studio Code');
 	let disposable = vscode.commands.registerCommand('vscode-go-check.helloWorld', () => {
 		vscode.window.showInformationMessage('Go Check for Visual Studio Code');
---
+++ src/run.ts
@@ -0,0 +1,146 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------*/
+
+'use strict';
+
+import vscode = require('vscode');
+import { CancellationToken, CodeLens, Command, TextDocument } from 'vscode';
+import { GoBaseCodeLensProvider } from 'Go/src/goBaseCodelens';
+import { GoDocumentSymbolProvider } from 'Go/src/goOutline';
+import { getBenchmarkFunctions } from 'Go/src/testUtils';
+import { getGoConfig } from 'Go/src/util';
+
+const testFuncRegex = /^Test.*|^Example.*/;
+const testMethodRegex = /^\(([^)]+)\)\.(Test.*)$/;
+
+// @TODO fix to handle methods
+export function getTestFunctions(
+    doc: vscode.TextDocument,
+    token: vscode.CancellationToken
+): Thenable<vscode.DocumentSymbol[]> {
+    const documentSymbolProvider = new GoDocumentSymbolProvider(true);
+    return documentSymbolProvider
+        .provideDocumentSymbols(doc, token)
+        .then((symbols) => symbols[0].children)
+        .then((symbols) => {
+            const testify = symbols.some(
+                (sym) => sym.kind === vscode.SymbolKind.Namespace && sym.name === '"github.com/stretchr/testify/suite"'
+            );
+            return symbols.filter(
+                (sym) =>
+                    sym.kind === vscode.SymbolKind.Method &&
+                    (testFuncRegex.test(sym.name) || (testify && testMethodRegex.test(sym.name)))
+            );
+        });
+}
+
+export class GoRunTestCodeLensProvider extends GoBaseCodeLensProvider {
+    private readonly benchmarkRegex = /^Benchmark.+/;
+
+    public async provideCodeLenses(document: TextDocument, token: CancellationToken): Promise<CodeLens[]> {
+        if (!this.enabled) {
+            return [];
+        }
+        const config = getGoConfig(document.uri);
+        const codeLensConfig = config.get<{ [key: string]: any }>('enableCodeLens');
+        const codelensEnabled = codeLensConfig ? codeLensConfig['runtest'] : false;
+        if (!codelensEnabled || !document.fileName.endsWith('_test.go')) {
+            return [];
+        }
+
+        const codelenses = await Promise.all([
+            this.getCodeLensForPackage(document, token),
+            this.getCodeLensForFunctions(document, token)
+        ]);
+        return ([] as CodeLens[]).concat(...codelenses);
+    }
+
+    private async getCodeLensForPackage(document: TextDocument, token: CancellationToken): Promise<CodeLens[]> {
+        const documentSymbolProvider = new GoDocumentSymbolProvider();
+        const symbols = await documentSymbolProvider.provideDocumentSymbols(document, token);
+        if (!symbols || symbols.length === 0) {
+            return [];
+        }
+        const pkg = symbols[0];
+        if (!pkg) {
+            return [];
+        }
+        const range = pkg.range;
+        const packageCodeLens = [
+            new CodeLens(range, {
+                title: 'run package tests',
+                command: 'go.test.package'
+            }),
+            new CodeLens(range, {
+                title: 'run file tests',
+                command: 'go.test.file'
+            })
+        ];
+        if (
+            pkg.children.some(
+                (sym) => sym.kind === vscode.SymbolKind.Function && this.benchmarkRegex.test(sym.name)
+            )
+        ) {
+            packageCodeLens.push(
+                new CodeLens(range, {
+                    title: 'run package benchmarks',
+                    command: 'go.benchmark.package'
+                }),
+                new CodeLens(range, {
+                    title: 'run file benchmarks',
+                    command: 'go.benchmark.file'
+                })
+            );
+        }
+        return packageCodeLens;
+    }
+
+    private async getCodeLensForFunctions(document: TextDocument, token: CancellationToken): Promise<CodeLens[]> {
+        const testPromise = async (): Promise<CodeLens[]> => {
+            const testFunctions = await getTestFunctions(document, token);
+            if (!testFunctions) {
+                return [];
+            }
+            const codelens: CodeLens[] = [];
+            for (const f of testFunctions) {
+                codelens.push(new CodeLens(f.range, {
+                    title: 'run test',
+                    command: 'go.test.cursor',
+                    arguments: [{ functionName: f.name }]
+                }));
+                codelens.push(new CodeLens(f.range, {
+                    title: 'debug test',
+                    command: 'go.debug.cursor',
+                    arguments: [{ functionName: f.name }]
+                }));
+            }
+            return codelens;
+        };
+
+        const benchmarkPromise = (async (): Promise<CodeLens[]> => {
+            const benchmarkFunctions = await getBenchmarkFunctions(document, token);
+            if (!benchmarkFunctions) {
+                return [];
+            }
+            const codelens: CodeLens[] = [];
+            for (const f of benchmarkFunctions) {
+                codelens.push(new CodeLens(f.range, {
+                    title: 'run benchmark',
+                    command: 'go.benchmark.cursor',
+                    arguments: [{ functionName: f.name }]
+                }));
+                codelens.push(new CodeLens(f.range, {
+                    title: 'debug benchmark',
+                    command: 'go.debug.cursor',
+                    arguments: [{ functionName: f.name }]
+                }));
+            }
+            return codelens;
+        });
+
+        const codelenses = await Promise.all([testPromise(), benchmarkPromise()]);
+        return ([] as CodeLens[]).concat(...codelenses);
+    }
+}
